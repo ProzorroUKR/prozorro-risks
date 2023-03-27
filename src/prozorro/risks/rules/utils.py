40360d751e5cac2aa387542185dc6729a53a2d04
Olha Zamirets <ozamirets@gmail.com>
2023-03-27 11:47:59 +0200

    Fix comparing dates

--- src/prozorro/risks/rules/utils.py
+++ src/prozorro/risks/rules/utils.py
@@ -4,4 +4,4 @@ from datetime import datetime
 def count_days_between_two_dates(date_1, date_2):
     date_1 = date_1 if isinstance(date_1, datetime) else datetime.fromisoformat(date_1)
     date_2 = date_2 if isinstance(date_2, datetime) else datetime.fromisoformat(date_2)
-    return (date_1 - date_2).days
+    return (date_1.date() - date_2.date()).days
