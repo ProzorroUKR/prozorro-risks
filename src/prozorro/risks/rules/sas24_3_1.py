a9b9c7cc632cad3080b116b4da57746c84a6a870
Olha Zamirets <ozamirets@gmail.com>
2024-05-16 17:04:03 +0300

    Risks sas24-3-10 and sas24-3-1 but without celery task

---
+++ src/prozorro/risks/rules/sas24_3_1.py
@@ -0,0 +1,106 @@
+from datetime import timedelta
+
+from prozorro.risks.models import RiskFound, RiskNotFound
+from prozorro.risks.rules.base import BaseTenderRiskRule
+from prozorro.risks.rules.utils import (
+    calculate_end_date,
+    get_complaints,
+    flatten,
+)
+from prozorro.risks.settings import SAS_24_RULES_FROM
+from prozorro.risks.utils import get_now
+
+DECISION_LIMIT = 30
+
+
+class RiskRule(BaseTenderRiskRule):
+    identifier = "sas24-3-1"
+    owner = "sas24"
+    name = "Невиконання замовником рішення органу оскарження"
+    description = (
+        "Індикатор свідчить про: Невиконання замовником рішення органу оскарження у встановлений законом термін."
+    )
+    legitimateness = (
+        "Стаття 18 частина 22: “Рішення органу оскарження набирають чинності з дня їх прийняття та є "
+        "обов’язковими для виконання замовниками, особами, яких вони стосуються. Якщо рішення органу "
+        "оскарження, прийняте за результатами розгляду органу оскарження, не було оскаржене до суду, "
+        "таке рішення має бути виконано не пізніше 30 днів з дня його прийняття органом оскарження.”"
+    )
+    development_basis = (
+        "Свідчить про ймовірне порушення вимог частини 22 статті 18 Закону. Автоматичний контроль "
+        "за виконанням рішень Органу оскаржень наразі відсутній в систем закупівель;"
+    )
+    procurement_methods = (
+        "negotiation",
+        "negotiation.quick",
+        "aboveThresholdEU",
+        "aboveThresholdUA",
+        "aboveThreshold",
+    )
+    tender_statuses = (
+        "active",
+        "active.tendering",
+        "active.pre-qualification.stand-still",
+        "active.pre-qualification",
+        "active.qualification",
+        "active.awarded",
+        "complete",
+    )
+    procuring_entity_kinds = (
+        "authority",
+        "central",
+        "general",
+        "social",
+        "special",
+        "defense",
+    )
+    value_for_services = 400000
+    value_for_works = 1500000
+    start_date = SAS_24_RULES_FROM
+
+    @staticmethod
+    def check_decision_delta(complaints):
+        for complaint in complaints:
+            # Якщо від complaints.dateDecision до поточної дати пройшло більше 30 днів, індикатор дорівнює 1.
+            calculated_date = calculate_end_date(complaint["dateDecision"], timedelta(days=DECISION_LIMIT))
+            if get_now() > calculated_date:
+                return RiskFound()
+        return RiskNotFound()
+
+    async def process_tender(self, tender, parent_object=None):
+        if self.tender_matches_requirements(tender, category=False, value=True):
+            complaints = get_complaints(tender, statuses=["satisfied"])
+            award_complaints = flatten(
+                [
+                    get_complaints(award, statuses=["satisfied"])
+                    for award in tender.get("awards", [])
+                ]
+            )
+            cancellation_complaints = flatten(
+                [
+                    get_complaints(cancellation, statuses=["satisfied"])
+                    for cancellation in tender.get("cancellations", [])
+                ]
+            )
+            qualifications_complaints = flatten(
+                [
+                    get_complaints(qualification, statuses=["satisfied"])
+                    for qualification in tender.get("qualifications", [])
+                ]
+            )
+
+            # Якщо в процедурі присутні блоки data.complaints, data.awards.complaints, data.qualification:complaints
+            # або data.cancellations:complaints. що мають complaints.type='complaint' та complaints.status = 'satisfied'
+            # то для кожного такого блоку порівнюємо complaints.dateDecision з поточною датой.
+            if qualifications_complaints:
+                return self.check_decision_delta(qualifications_complaints)
+
+            if complaints:
+                return self.check_decision_delta(complaints)
+
+            if award_complaints:
+                return self.check_decision_delta(award_complaints)
+
+            if cancellation_complaints:
+                return self.check_decision_delta(cancellation_complaints)
+        return RiskNotFound()
---
+++ tests/integration/rules/test_rule_24_3_10.py
@@ -0,0 +1,158 @@
+from datetime import timedelta
+
+import pytest
+from copy import deepcopy
+
+from prozorro.risks.models import RiskFound, RiskNotFound
+from prozorro.risks.rules.sas24_3_10 import RiskRule
+from prozorro.risks.utils import get_now
+from tests.integration.conftest import get_fixture_json
+
+
+tender_data = get_fixture_json("base_tender")
+tender_data["procuringEntity"]["kind"] = "general"
+tender_data["mainProcurementCategory"] = "works"
+tender_data["value"]["amount"] = 1600000
+tender_data.update(
+    {
+        "procurementMethodType": "aboveThreshold",
+        "status": "active.qualification",
+        "lots": [
+            {
+                "title": "Бетон та розчин будівельний",
+                "status": "active",
+                "id": "c2bb6ff3e8e547bee11d8bff23e8a295",
+                "date": "2023-02-21T17:36:11.836903+02:00",
+                "value": {
+                    "amount": 168233.0,
+                    "currency": "UAH",
+                    "valueAddedTaxIncluded": True,
+                },
+                "minimalStep": {
+                    "amount": 1682.0,
+                    "currency": "UAH",
+                    "valueAddedTaxIncluded": True,
+                },
+                "guarantee": {"amount": 0.0, "currency": "UAH"},
+            }
+        ],
+    }
+)
+tender_data["awards"][0].update({
+    "date": (get_now() - timedelta(days=31)).isoformat(),
+    "status": "active",
+    "bidID": tender_data["bids"][0]["id"],
+    "lotID": tender_data["lots"][0]["id"],
+})
+
+
+async def test_open_eu_tender_has_risk():
+    risk_rule = RiskRule()
+    result = await risk_rule.process_tender(tender_data)
+    assert result == RiskFound()
+
+
+async def test_tender_without_lots_has_risk():
+    tender = deepcopy(tender_data)
+    tender.pop("lots")
+    risk_rule = RiskRule()
+    result = await risk_rule.process_tender(tender)
+    assert result == RiskFound()
+
+
+@pytest.mark.parametrize(
+    "amount,category,risk_rule_class,risk_result",
+    [
+        (40000, "services", RiskRule, RiskNotFound()),
+        (400000, "services", RiskRule, RiskFound()),
+        (1600000, "works", RiskRule, RiskFound()),
+        (100000, "works", RiskRule, RiskNotFound()),
+        (500000, "goods", RiskRule, RiskFound()),
+        (20000, "goods", RiskRule, RiskNotFound()),
+    ],
+)
+async def test_tender_value(amount, category, risk_rule_class, risk_result):
+    tender = deepcopy(tender_data)
+    tender.update(
+        {
+            "procurementMethodType": "aboveThresholdEU",
+            "mainProcurementCategory": category,
+        }
+    )
+    tender["value"]["amount"] = amount
+    risk_rule = risk_rule_class()
+    result = await risk_rule.process_tender(tender)
+    assert result == risk_result
+
+
+async def test_tender_bidder_has_no_eligibility_docs():
+    tender = deepcopy(tender_data)
+    tender["bids"][0].pop("documents")
+    risk_rule = RiskRule()
+    result = await risk_rule.process_tender(tender)
+    assert result == RiskFound()
+
+
+async def test_tender_bidder_has_eligibility_docs():
+    tender = deepcopy(tender_data)
+    tender["bids"][0]["documents"][0]["documentType"] = "eligibilityDocuments"
+    risk_rule = RiskRule()
+    result = await risk_rule.process_tender(tender)
+    assert result == RiskNotFound()
+
+
+async def test_tender_bidder_has_eligibility_docs_in_another_envelopes():
+    tender = deepcopy(tender_data)
+    tender["bids"][0]["eligibilityDocuments"] = [tender["bids"][0]["documents"][0]]
+    tender["bids"][0].pop("documents")
+    tender["bids"][0]["eligibilityDocuments"][0]["documentType"] = "eligibilityDocuments"
+    risk_rule = RiskRule()
+    result = await risk_rule.process_tender(tender)
+    assert result == RiskNotFound()
+
+
+async def test_tender_with_cancelled_award_lot():
+    tender = deepcopy(tender_data)
+    tender["lots"][0]["status"] = "cancelled"
+    risk_rule = RiskRule()
+    result = await risk_rule.process_tender(tender)
+    assert result == RiskNotFound()
+
+
+@pytest.mark.parametrize(
+    "winner_date,risk_result",
+    [
+        ((get_now() - timedelta(days=6)).isoformat(), RiskFound()),
+        ((get_now() - timedelta(days=2)).isoformat(), RiskNotFound()),
+    ],
+)
+async def test_tender_check_winner_duration(winner_date, risk_result):
+    tender = deepcopy(tender_data)
+    tender["awards"][0]["date"] = winner_date
+    risk_rule = RiskRule()
+    result = await risk_rule.process_tender(tender)
+    assert result == risk_result
+
+
+async def test_tender_with_not_risky_tender_status():
+    tender = deepcopy(tender_data)
+    tender["status"] = "cancelled"
+    risk_rule = RiskRule()
+    result = await risk_rule.process_tender(tender)
+    assert result == RiskNotFound()
+
+
+async def test_tender_with_not_risky_procurement_type():
+    tender = deepcopy(tender_data)
+    tender["procurementMethodType"] = "reporting"
+    risk_rule = RiskRule()
+    result = await risk_rule.process_tender(tender)
+    assert result == RiskNotFound()
+
+
+async def test_tender_with_not_risky_procurement_entity_kind():
+    tender = deepcopy(tender_data)
+    tender["procuringEntity"]["kind"] = "other"
+    risk_rule = RiskRule()
+    result = await risk_rule.process_tender(tender)
+    assert result == RiskNotFound()
